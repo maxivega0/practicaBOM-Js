05f1551dc71177279d3233d254654a0af9a8e004
maxivega0 <maximilianovega600@gmail.com>
2023-04-13 17:18:32 -0300

    agregamos colores al texto y hora, y colores a los botones

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // Window es objeto del BOM
 // setTimeout -> ejecuta una funcion "x" tiempo en ms.
-let contador = 1;
-function mensaje() {
-    document.write(`<p>Esta es una prueba de setTimeout, N= ${contador}</p>`);
+// let contador = 1;
+// function mensaje() {
+//     document.write(`<p>Esta es una prueba de setTimeout, N= ${contador}</p>`);
     
-    if (contador === 6) {
-        //Logica para detener un setInterval ((Para eso creamos la variable)
-        clearInterval(idInterval);
-    }
-    contador++;
-}
+//     if (contador === 6) {
+//         //Logica para detener un setInterval ((Para eso creamos la variable)
+//         clearInterval(idInterval);
+//     }
+//     contador++;
+// }
 
 // 2 argumentos, primero la funcion, luego el tiempo en ms 
 // window.setTimeout(mensaje, 3000);
@@ -20,9 +20,9 @@ function mensaje() {
 
 // setInterval -> Ejecuta una funcion cada "x" tiempo en ms
 
-setInterval(mensaje,2000);
-// setInterval(() => {
-//     document.write(`<p>Esta es una nueva prueba</p>`)
-// }, 1000); 
-//* almacenamos en una variable para detenerlo
-const idInterval = setInterval(mensaje,2000);
+// setInterval(mensaje,2000);
+// // setInterval(() => {
+// //     document.write(`<p>Esta es una nueva prueba</p>`)
+// // }, 1000); 
+// //* almacenamos en una variable para detenerlo
+// const idInterval = setInterval(mensaje,2000);
